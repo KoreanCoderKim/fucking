a5c6365654d94116ae54b79b59585b1d6d107f8f
KimJY <sagwai141@gmail.com>
2025-07-21 21:02:46 +0900

    Delete src/main/java/io/cloudtype/Demo directory

--- src/main/java/io/cloudtype/Demo/RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Article;
-
-import jakarta.persistence.Table;
-import org.springframework.data.repository.CrudRepository;
-@Table(name="Us")
-public interface RoomRepository extends CrudRepository<Room, Long> {
-    boolean existsByRoomId(String roomId);
-}

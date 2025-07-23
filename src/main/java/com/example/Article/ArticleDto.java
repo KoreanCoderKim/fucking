5695b4c67ab85b0a349859f1121381f072d889f0
KimJY <sagwai141@gmail.com>
2025-07-23 11:10:20 +0900

    Update ArticleDto.java

--- src/main/java/com/example/Article/ArticleDto.java
+++ src/main/java/com/example/Article/ArticleDto.java
@@ -18,12 +18,12 @@ public class ArticleDto {
     @Column
     private String News;
 
-    public ArticleDto(String userId, int password, String title, String News) {
+    public ArticleDto(String userId, Object password, String title, String News) {
         this.title = title;
         this.News = News;
     }
 
-    public Article toEntity(String Data, String usId, int password) {
+    public Article toEntity(String Data, String usId, Object password) {
         return new Article(null, usId, password, Data, title, News);
     }
 }

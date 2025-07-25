2eb96dc3f3909e36c5541a1c465cbe95110b1e6e
KimJY <sagwai141@gmail.com>
2025-07-25 17:48:05 +0900

    Update UserDto.java

--- src/main/java/com/example/Article/UserDto.java
+++ src/main/java/com/example/Article/UserDto.java
@@ -20,7 +20,9 @@ public class UserDto {
         this.userId = userId;
         this.password = password;
     }
-
+    public void setPassword(String password) {
+        this.password = password;
+    }
     @Override
     public String toString() {
         return "UserDto{" +

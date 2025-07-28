a7f5fc0c87a65bacaf3daae4d74c3007b702f201
KimJY <sagwai141@gmail.com>
2025-07-28 22:11:17 +0900

    Update ReplyRepository.java

--- src/main/java/com/example/Article/ReplyRepository.java
+++ src/main/java/com/example/Article/ReplyRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface ReplyRepository extends CrudRepository<Reply, Long> {
     List<Reply> findByCommentId(Long commentId);
+    List<Reply> findByUsName(String usName);
 }

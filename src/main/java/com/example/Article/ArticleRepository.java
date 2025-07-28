6dd02851346b9647191ac2962475d02174d1ce23
KimJY <sagwai141@gmail.com>
2025-07-28 18:01:27 +0900

    Update ArticleRepository.java

--- src/main/java/com/example/Article/ArticleRepository.java
+++ src/main/java/com/example/Article/ArticleRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findByRoomId(String RoomId);
+    List<Article> findByUsId(String usId);
 }

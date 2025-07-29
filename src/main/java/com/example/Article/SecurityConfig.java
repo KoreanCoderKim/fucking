94ee02f819aa507586e24f4d7b9ce860f0fb5d96
KimJY <sagwai141@gmail.com>
2025-07-29 12:42:03 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/Article/SecurityConfig.java
+++ src/main/java/com/example/Article/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.Article;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +13,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .headers(headers -> headers
+                .frameOptions(frame -> frame.sameOrigin()) // iframe 같은 도메인 허용
+            )
             .authorizeHttpRequests(customizer -> customizer
                 .anyRequest().permitAll()
             )

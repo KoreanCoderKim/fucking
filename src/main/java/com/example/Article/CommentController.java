587bd943737b195d8b66c404cf9cb6e532e76599
KimJY <sagwai141@gmail.com>
2025-08-02 20:10:50 +0900

    Create CommentController.java

---
+++ src/main/java/com/example/Article/CommentController.java
@@ -0,0 +1,34 @@
+package com.example.Article;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+public class CommentController {
+    @Autowired
+    CommentRepository commentRepository;
+    @Autowired
+    ArticleRepository articleRepository;
+    @GetMapping("/Comment")
+    public String commented(@RequestParam Long articleId, Model model) {
+        model.addAttribute("ai",articleId);
+        return "Comment";
+    }
+    @PostMapping("/Commented")
+    public String commneting(@RequestParam Long aid, CommentDto form, HttpSession session) {
+        form.setUsName((String) session.getAttribute("user"));
+        form.setArticleId(aid);
+        Comment comment = form.toEntity();
+        commentRepository.save(comment);
+        Optional<Article> article = articleRepository.findById(aid);
+        String RoomId = article.get().getRoomId();
+        return "redirect:index?RoomId="+RoomId+"&Page=1";
+    }
+}

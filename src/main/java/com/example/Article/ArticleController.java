388e51c47c42d34a7c0764d7d51a2f926528ffe0
KimJY <sagwai141@gmail.com>
2025-08-02 21:58:04 +0900

    ArticleController.java 업데이트

--- src/main/java/com/example/Article/ArticleController.java
+++ src/main/java/com/example/Article/ArticleController.java
@@ -112,10 +112,10 @@ public class ArticleController {
             comments = List.of();
         else if (comment.size() <= 5) 
             comments = comment;
-        else if (comment.size() < (Page - 1) * 5 + 10)
+        else if (comment.size() < (Page - 1) * 5 + 5)
             comments = comment.subList((Page - 1) * 5, comment.size());
         else
-            comments = comment.subList((Page - 1) * 5, (Page-1)*5+10);
+            comments = comment.subList((Page - 1) * 5, (Page-1)*5+5);
         model.addAttribute("ID", id);
         model.addAttribute("data", article.get().getNews());
         model.addAttribute("data2",comments);

fe7d37b183a8529e89436a917ecd6a61b4e2aa8a
KimJY <sagwai141@gmail.com>
2025-08-02 21:01:31 +0900

    Update ArticleController.java

--- src/main/java/com/example/Article/ArticleController.java
+++ src/main/java/com/example/Article/ArticleController.java
@@ -97,9 +97,12 @@ public class ArticleController {
         Optional<Article> article = articleRepository.findById(id);
         List<Comment> comment = commentRepository.findByArticleId(id);
         List<Integer> Number = List.of();
+        int sizer = 0;
         if (comment.size() <= 5)
-            Number.add(1);
-        for (int i = 1; i <= (comment.size() / 5); i++) {
+            sizer = 1;
+        else
+            sizer = comment.size() / 5;
+        for (int i = 1; i <= sizer; i++) {
             Number.add(i);
         }
         List<Comment> comments = List.of();

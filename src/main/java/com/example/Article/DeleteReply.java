b3b3a0c8b107cd7e61a0c645c363c83f3faeab60
KimJY <sagwai141@gmail.com>
2025-08-09 20:36:29 +0900

    Update DeleteReply.java

--- src/main/java/com/example/Article/DeleteReply.java
+++ src/main/java/com/example/Article/DeleteReply.java
@@ -19,10 +19,11 @@ public class DeleteReply {
             comment.get().updateRV("삭제된 답글");
             comment.get().NotAcceptedReply();
             replyRepository.save(new Reply(comment.get().getId(), comment.get().getCommentId(), comment.get().getUsName(), comment.get().getReplyValue(), comment.get().getisMode()));
+            return "redirect:/In";
         }
         else {
             return "redirect:/404";
         }
-        return "";
     }
 }
+

f1ee959ba5ccde7642e64475720e84ca01555544
KimJY <sagwai141@gmail.com>
2025-08-09 20:43:47 +0900

    Update DeleteComment.java

--- src/main/java/com/example/Article/DeleteComment.java
+++ src/main/java/com/example/Article/DeleteComment.java
@@ -19,11 +19,12 @@ public class DeleteComment {
             comment.get().updateCV("삭제된 댓글");
             comment.get().NotAcceptedReply();
             commentRepository.save(new Comment(comment.get().getId(), comment.get().getArticleId(), comment.get().getUsName(), comment.get().getCommentValue(), comment.get().getisMode()));
-            return "redirect:/In";
+            return "redirect:/Inside?id={{id}}&Page=1";
         }
         else {
             return "redirect:/404";
         }
     }
 }
+
 

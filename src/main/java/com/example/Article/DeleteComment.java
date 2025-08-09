d5e5c9028e7af3e482be5778b59298e76362bd88
KimJY <sagwai141@gmail.com>
2025-08-09 20:27:09 +0900

    Add files via upload

---
+++ src/main/java/com/example/Article/DeleteComment.java
@@ -0,0 +1,28 @@
+package com.example.Article;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+public class DeleteComment {
+    @Autowired
+    CommentRepository commentRepository;
+
+    @PostMapping("/DelComm")
+    public String DelComm(@RequestParam Long id) {
+        Optional<Comment> comment = commentRepository.findById(id);
+        if (comment.get().getisMode()) {
+            comment.get().updateCV("삭제된 댓글");
+            comment.get().NotAcceptedReply();
+            commentRepository.save(new Comment(comment.get().getId(), comment.get().getArticleId(), comment.get().getUsName(), comment.get().getCommentValue(), comment.get().getisMode()));
+        }
+        else {
+            return "redirect:/404";
+        }
+        return "";
+    }
+}
---
+++ src/main/java/com/example/Article/DeleteReply.java
@@ -0,0 +1,28 @@
+package com.example.Article;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+public class DeleteReply {
+    @Autowired
+    ReplyRepository replyRepository;
+
+    @PostMapping("/DelRep")
+    public String DelRep(@RequestParam Long id) {
+        Optional<Reply> comment = replyRepository.findById(id);
+        if (comment.get().getisMode()) {
+            comment.get().updateRV("삭제된 답글");
+            comment.get().NotAcceptedReply();
+            replyRepository.save(new Reply(comment.get().getId(), comment.get().getCommentId(), comment.get().getUsName(), comment.get().getReplyValue(), comment.get().getisMode()));
+        }
+        else {
+            return "redirect:/404";
+        }
+        return "";
+    }
+}

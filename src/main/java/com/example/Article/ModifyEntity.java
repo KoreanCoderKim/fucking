044ef8d2a2cade3dc0a3a21302380acf8b3b5cb4
KimJY <sagwai141@gmail.com>
2025-08-10 16:46:29 +0900

    Update ModifyEntity.java

--- src/main/java/com/example/Article/ModifyEntity.java
+++ src/main/java/com/example/Article/ModifyEntity.java
@@ -1,1 +1,31 @@
+package com.example.Article;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Getter
+@NoArgsConstructor
+@Entity
+public class ModifyEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column
+    private String Value;
+
+    public ModifyEntity(Long id, String Value) {
+        this.id = id;
+        this.Value = Value;
+    }
+
+    @Override
+    public String toString() {
+        return "ModifyEntity{" +
+                ", id='" + id + '\'' +
+                ", Value=" + Value + '\'' +
+                '}';
+    }
+}

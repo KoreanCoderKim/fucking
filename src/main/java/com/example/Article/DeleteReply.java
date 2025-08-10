7556ca095342375e47b8d0157d6b019b393fefdf
KimJY <sagwai141@gmail.com>
2025-08-10 17:15:50 +0900

    Update DeleteReply.java

--- src/main/java/com/example/Article/DeleteReply.java
+++ src/main/java/com/example/Article/DeleteReply.java
@@ -1,5 +1,5 @@
 package com.example.Article;
-
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,6 +39,7 @@ public class DeleteReply {
         }
     }
 }
+
 
 
 

594475cebbee705c90f652b722c90bea4aa4fb0e
KimJY <sagwai141@gmail.com>
2025-08-11 20:34:54 +0900

    Update UserController.java

--- src/main/java/com/example/Article/UserController.java
+++ src/main/java/com/example/Article/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Controller
@@ -81,4 +81,5 @@ public class UserController {
         return "redirect:/Login?SessionState=Good";
     }
 }
+
 

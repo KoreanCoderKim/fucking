0e5aff8f9c3d020aebcd56f2666af913b6ffb683
KimJY <sagwai141@gmail.com>
2025-08-11 20:32:59 +0900

    Update DeleteController.java

--- src/main/java/com/example/Article/DeleteController.java
+++ src/main/java/com/example/Article/DeleteController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 @Controller

0315a9a805ca5e27171f3b062f6015e48248cf8c
KimJY <sagwai141@gmail.com>
2025-08-11 20:35:20 +0900

    Update UserRepository.java

--- src/main/java/com/example/Article/UserRepository.java
+++ src/main/java/com/example/Article/UserRepository.java
@@ -8,10 +8,11 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     @Lock(LockModeType.PESSIMISTIC_READ)
     @Query("select a from User a where a.id = :id")
-    User findByIdForUpdate(Long id);
+    User findByIdForUpdate(@Param("id") Long id);
     boolean existsByUsId(String usId);
 
     List<User> findByUsId(String usId);
 }
+
 
 

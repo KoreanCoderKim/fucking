12f871b673529c903459ee9c6737bc2db5b014ea
KimJY <sagwai141@gmail.com>
2025-08-11 20:21:27 +0900

    Update ReplyRepository.java

--- src/main/java/com/example/Article/ReplyRepository.java
+++ src/main/java/com/example/Article/ReplyRepository.java
@@ -7,9 +7,10 @@ import java.util.List;
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
     @Lock(LockModeType.PESSIMISTIC_READ)
     @Query("select a from Reply a where a.id = :id")
-    Article findByIdForUpdate(Long id);
+    Reply findByIdForUpdate(Long id);
     List<Reply> findByCommentId(Long commentId);
     List<Reply> findByUsName(String usName);
 }
+
 
 

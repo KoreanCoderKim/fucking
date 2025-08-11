ff810507c30a4272239c4df298be4996f136ed61
KimJY <sagwai141@gmail.com>
2025-08-11 20:13:16 +0900

    Update ArticleRepository.java

--- src/main/java/com/example/Article/ArticleRepository.java
+++ src/main/java/com/example/Article/ArticleRepository.java
@@ -6,10 +6,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Lock(LockModeType.PESSIMISTIC_READ)
     @Query("select a from Article a where a.id = :id")
     Article findByIdForUpdate(Long id);
     List<Article> findByRoomId(String RoomId);
     List<Article> findByUsId(String usId);
 }
+
 

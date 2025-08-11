164cda6a065c2e108d28b37b29cd150ae4bef11c
KimJY <sagwai141@gmail.com>
2025-08-11 20:21:45 +0900

    Update RoomRepository.java

--- src/main/java/com/example/Article/RoomRepository.java
+++ src/main/java/com/example/Article/RoomRepository.java
@@ -4,9 +4,10 @@ import jakarta.persistence.Table;
 import org.springframework.data.repository.JpaRepository;
 @Table(name="Us")
 public interface RoomRepository extends JpaRepository<Room, Long> {
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Lock(LockModeType.PESSIMISTIC_READ)
     @Query("select r from Room r where r.id = :id")
     Room findByIdForUpdate(Long id);
     boolean existsByRoomId(String roomId);
 }
+
 

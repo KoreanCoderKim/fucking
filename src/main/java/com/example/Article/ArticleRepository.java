51eef6d911347a4b716a09c606c87735f35a8db8
KimJY <sagwai141@gmail.com>
2025-08-11 20:44:49 +0900

    Update ArticleRepository.java

--- src/main/java/com/example/Article/ArticleRepository.java
+++ src/main/java/com/example/Article/ArticleRepository.java
@@ -3,7 +3,7 @@ package com.example.Article;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +15,7 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findByRoomId(String RoomId);
     List<Article> findByUsId(String usId);
 }
+
 
 
 

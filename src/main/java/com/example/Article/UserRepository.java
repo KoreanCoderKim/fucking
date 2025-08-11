abbe926adc00ce9a8f66656eee3995db474856d7
KimJY <sagwai141@gmail.com>
2025-08-11 20:43:50 +0900

    Update UserRepository.java

--- src/main/java/com/example/Article/UserRepository.java
+++ src/main/java/com/example/Article/UserRepository.java
@@ -3,7 +3,7 @@ package com.example.Article;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -17,6 +17,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findByUsId(String usId);
 }
+
 
 
 

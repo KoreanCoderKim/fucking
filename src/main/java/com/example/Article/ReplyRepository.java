a8100b49983f593b4daaedc3c4d24934c9a26a93
KimJY <sagwai141@gmail.com>
2025-08-11 20:44:14 +0900

    Update ReplyRepository.java

--- src/main/java/com/example/Article/ReplyRepository.java
+++ src/main/java/com/example/Article/ReplyRepository.java
@@ -3,7 +3,7 @@ package com.example.Article;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -15,6 +15,7 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
     List<Reply> findByCommentId(Long commentId);
     List<Reply> findByUsName(String usName);
 }
+
 
 
 

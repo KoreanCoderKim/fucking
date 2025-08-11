1e93d042a3257fd0fa49628d1cec228e0a9de7cf
KimJY <sagwai141@gmail.com>
2025-08-11 20:35:03 +0900

    Update UserDeleteController.java

--- src/main/java/com/example/Article/UserDeleteController.java
+++ src/main/java/com/example/Article/UserDeleteController.java
@@ -4,7 +4,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Controller
@@ -46,4 +46,5 @@ public class UserDeleteController {
         return "redirect:/Main";
     }
 }
+
 

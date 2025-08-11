b410872d2d9b732952259240131243ebcc25e44e
KimJY <sagwai141@gmail.com>
2025-08-11 20:32:45 +0900

    Update CommentRepository.java

--- src/main/java/com/example/Article/CommentRepository.java
+++ src/main/java/com/example/Article/CommentRepository.java
@@ -7,9 +7,10 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Lock(LockModeType.PESSIMISTIC_READ)
     @Query("select a from Comment a where a.id = :id")
-    Comment findByIdForUpdate(Long id);
+    Comment findByIdForUpdate(@Param("id") Long id);
     List<Comment> findByArticleId(Long articleId);
     List<Comment> findByUsName(String usName);
 }
+
 
 

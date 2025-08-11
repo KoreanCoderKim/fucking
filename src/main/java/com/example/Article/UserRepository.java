1de0218fb4f4336d5221c24fcb67e3785a6db909
KimJY <sagwai141@gmail.com>
2025-08-11 19:47:56 +0900

    Update UserRepository.java

--- src/main/java/com/example/Article/UserRepository.java
+++ src/main/java/com/example/Article/UserRepository.java
@@ -1,12 +1,16 @@
 package com.example.Article;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select a from User a where a.id = :id")
+    Article findByIdForUpdate(Long id);
     boolean existsByUsId(String usId);
 
     List<User> findByUsId(String usId);
 }
+

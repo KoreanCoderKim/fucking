d27734ce7e65aac30ac6f038e382fe0982f043fa
KimJY <sagwai141@gmail.com>
2025-08-11 20:32:13 +0900

    Update DeleteComment.java

--- src/main/java/com/example/Article/DeleteComment.java
+++ src/main/java/com/example/Article/DeleteComment.java
@@ -1,5 +1,5 @@
 package com.example.Article;
-
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,6 +41,7 @@ public class DeleteComment {
         }
     }
 }
+
 
 
 

e8826e07e269cc0b202bf9c33868b7652c2c498f
KimJY <sagwai141@gmail.com>
2025-08-11 20:33:24 +0900

    Update ModifyComment.java

--- src/main/java/com/example/Article/ModifyComment.java
+++ src/main/java/com/example/Article/ModifyComment.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 @Controller
@@ -50,3 +50,4 @@ public class ModifyComment {
     }
 
 }
+

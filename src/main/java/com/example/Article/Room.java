c2b0f027aaae523502523754bb3cae3e8f4d27f6
KimJY <sagwai141@gmail.com>
2025-08-13 12:37:28 +0900

    Room.java 업데이트

--- src/main/java/com/example/Article/Room.java
+++ src/main/java/com/example/Article/Room.java
@@ -3,7 +3,7 @@ package com.example.Article;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,7 +14,7 @@ public class Room {
     @Id
     @GeneratedValue
     private Long id;
-    @Column
+    @Column(unique = true, nullable = false)
     private String roomId;
 
     public Room(Long id, String roomId) {
